0c45546e452b4199eb7efbaa8c7dd618de1730ce
Airaj24 <rajasekarellappan24@gmail.com>
2025-03-27 14:47:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "dev-airaj24"
+    bucket = "raj-s3-jenkins"
     key    = "raj/terraform.tfstate"
     region = "us-east-1"   
   }
